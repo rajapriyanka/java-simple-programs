23c1b3f4495eed92406e465e6181206edb13d8e7
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-06-17 06:56:10 +0530

    Update 1061. Lexicographically Smallest Equivalent String Medium Union Find String.java

--- 1061. Lexicographically Smallest Equivalent String Medium Union Find String.java
+++ 1061. Lexicographically Smallest Equivalent String Medium Union Find String.java
@@ -1,7 +1,6 @@
 class Solution {
     private int[] parent;
     public String smallestEquivalentString(String s1, String s2, String baseStr) {
-
         parent = new int[26];
         for (int i = 0; i < 26; ++i) {
             parent[i] = i;
