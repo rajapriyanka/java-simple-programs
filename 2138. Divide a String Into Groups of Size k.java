169f603aafbb6c38baf5b3c7a9f7178f340748d8
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-06-22 07:55:30 +0530

    Update 2138. Divide a String Into Groups of Size k.java

--- 2138. Divide a String Into Groups of Size k.java
+++ 2138. Divide a String Into Groups of Size k.java
@@ -1,7 +1,6 @@
 class Solution {
   public String[] divideString(String s, int k, char fill) {
     String[] ans = new String[(s.length() + k - 1) / k];
-
     
     for (int i = 0, j = 0; i < s.length(); i += k)
       ans[j++] = i + k > s.length()
