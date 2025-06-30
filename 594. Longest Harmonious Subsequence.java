5d5d3d1e4d8fd72dd8dcffcc6c835d985a6a4de7
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-06-30 06:19:09 +0530

    Update 594. Longest Harmonious Subsequence.java

--- 594. Longest Harmonious Subsequence.java
+++ 594. Longest Harmonious Subsequence.java
@@ -4,7 +4,6 @@ class Solution {
     Map<Integer, Integer> count = new HashMap<>();
     for (final int num : nums)
       count.merge(num, 1, Integer::sum);
-
     for (final int num : count.keySet())
       if (count.containsKey(num + 1))
         ans = Math.max(ans, count.get(num) + count.get(num + 1));
