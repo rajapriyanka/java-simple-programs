dd9ad6ccf7ba657bf4628fc15c25e3314fa6d03c
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-07-11 06:42:18 +0530

    Update 2402. Meeting Rooms III.java

--- 2402. Meeting Rooms III.java
+++ 2402. Meeting Rooms III.java
@@ -15,8 +15,7 @@ class Solution {
     for (int[] meeting : meetings) {
       final int start = meeting[0];
       final int end = meeting[1];
-      // Push meetings ending before this `meeting` in occupied to the
-      // `availableRoomsIds`.
+     
       while (!occupied.isEmpty() && occupied.peek().endTime <= start)
         availableRoomIds.offer(occupied.poll().roomId);
       if (availableRoomIds.isEmpty()) {
