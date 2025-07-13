ba123aa4c78d6f8a2f3325437d38661f5ef055a6
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-07-13 07:32:03 +0530

    Update 2410. Maximum Matching of Players With Trainers.java

--- 2410. Maximum Matching of Players With Trainers.java
+++ 2410. Maximum Matching of Players With Trainers.java
@@ -3,7 +3,6 @@ class Solution {
     int ans = 0;
     Arrays.sort(players);
     Arrays.sort(trainers);
-
     for (int i = 0; i < trainers.length; ++i)
       if (players[ans] <= trainers[i] && ++ans == players.length)
         return ans;
