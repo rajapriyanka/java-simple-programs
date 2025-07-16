ded4bbc272c96f79ebacfb9c3dcdf00f8ad7e612
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-07-16 06:29:59 +0530

    Update 3201. Find the Maximum Length of Valid Subsequence I.java

--- 3201. Find the Maximum Length of Valid Subsequence I.java
+++ 3201. Find the Maximum Length of Valid Subsequence I.java
@@ -1,6 +1,6 @@
 class Solution {
   public int maximumLength(int[] nums) {
-    // dp[i][j] := the maximum length of a valid subsequence, where the last
+   
     // number mod 2 equal to i and the next desired number mod 2 equal to j
     int[][] dp = new int[k][k];
 
