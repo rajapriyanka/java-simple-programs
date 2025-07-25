d840719f7af2c612de6d6312d9336faa265e6b90
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-07-25 06:46:29 +0530

    Update 3487. Maximum Unique Subarray Sum After Deletion.java

--- 3487. Maximum Unique Subarray Sum After Deletion.java
+++ 3487. Maximum Unique Subarray Sum After Deletion.java
@@ -3,8 +3,7 @@ class Solution {
     final int mx = Arrays.stream(nums).max().getAsInt();
     if (mx <= 0)
       return mx;
-    return Arrays.stream(nums)
-        .boxed()
+    return Arrays.stream(nums).boxed()
         .collect(Collectors.toSet())
         .stream()
         .filter(num -> num > 0)
