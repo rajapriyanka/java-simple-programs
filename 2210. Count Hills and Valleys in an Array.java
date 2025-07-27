76d4e8710db79c513e79f94e83c44f06ccbf8867
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-07-27 08:08:15 +0530

    Update 2210. Count Hills and Valleys in an Array.java

--- 2210. Count Hills and Valleys in an Array.java
+++ 2210. Count Hills and Valleys in an Array.java
@@ -3,7 +3,7 @@ class Solution {
     int ans = 0;
     int left = nums[0];
     for (int i = 1; i + 1 < nums.length; ++i)
-      if (left < nums[i] && nums[i] > nums[i + 1] || // the hill
+      if (left < nums[i] && nums[i] > nums[i + 1] ||
           left > nums[i] && nums[i] < nums[i + 1]) { // the valley
         ++ans;
         left = nums[i];
