acf40932834a3e13259deaea8b2164f11ede6a66
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-07-31 07:01:06 +0530

    Update 898. Bitwise ORs of Subarrays.java

--- 898. Bitwise ORs of Subarrays.java
+++ 898. Bitwise ORs of Subarrays.java
@@ -2,7 +2,6 @@ class Solution {
   public int subarrayBitwiseORs(int[] arr) {
     List<Integer> s = new ArrayList<>();
     int l = 0;
-
     for (final int a : arr) {
       final int r = s.size();
       s.add(a);
