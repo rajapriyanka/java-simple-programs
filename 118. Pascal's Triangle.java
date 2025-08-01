e29649cd4f09b96d14b2a635615c2ddcafd89537
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-08-01 07:23:43 +0530

    Update 118. Pascal's Triangle.java

--- 118. Pascal's Triangle.java
+++ 118. Pascal's Triangle.java
@@ -9,7 +9,6 @@ class Solution {
     for (int i = 2; i < numRows; ++i)
       for (int j = 1; j < ans.get(i).size() - 1; ++j)
         ans.get(i).set(j, ans.get(i - 1).get(j - 1) + ans.get(i - 1).get(j));
-
     return ans;
   }
 }
