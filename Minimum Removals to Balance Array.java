432ba934091abe2da809ff3ca3732b01b6920b97
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-08-02 20:30:10 +0530

    Create Minimum Removals to Balance Array.java

---
+++ Minimum Removals to Balance Array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minRemoval(int[] nums, int k) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        int minRemovals = n;
+
+        for (int i = 0; i < n; i++) {
+            long maxAllowed = (long) nums[i] * k;
+
+            int low = i, high = n - 1, right = i;
+            while (low <= high) {
+                int mid = low + (high - low) / 2;
+                if (nums[mid] <= maxAllowed) {
+                    right = mid;
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+
+            int subarraySize = right - i + 1;
+            int removals = n - subarraySize;
+            minRemovals = Math.min(minRemovals, removals);
+        }
+
+        return minRemovals;
+    }
+}
