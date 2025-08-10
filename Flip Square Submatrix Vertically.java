190aa286a73a73e591b23b7c03f9de7425a6b419
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-08-10 08:23:41 +0530

    Update Flip Square Submatrix Vertically.java

--- Flip Square Submatrix Vertically.java
+++ Flip Square Submatrix Vertically.java
@@ -1,7 +1,6 @@
 class Solution {
     public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
-        // Flip the submatrix vertically by reversing the order of rows within the submatrix
-        for (int i = 0; i < k / 2; i++) {
+              for (int i = 0; i < k / 2; i++) {
             int topRow = x + i;
             int bottomRow = x + k - 1 - i;
             for (int col = y; col < y + k; col++) {
