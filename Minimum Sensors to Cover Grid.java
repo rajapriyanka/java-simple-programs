c3fd3294d47363b1f4bf2f0bd27e28beb4c2fe3f
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-08-16 20:08:11 +0530

    Update Minimum Sensors to Cover Grid.java

--- Minimum Sensors to Cover Grid.java
+++ Minimum Sensors to Cover Grid.java
@@ -1,7 +1,7 @@
 class Solution {
     public int minSensors(int n, int m, int k) {
         int side = 2 * k + 1;  
-        int rows = (n + side - 1) / side; // ceil division
+        int rows = (n + side - 1) / side;
         int cols = (m + side - 1) / side; // ceil division
         return rows * cols;
     }
