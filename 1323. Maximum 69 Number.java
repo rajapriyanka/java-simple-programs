2e1bee4bfd3ffbd67a27c17af5cc776bb820d77e
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-08-16 08:56:17 +0530

    Update 1323. Maximum 69 Number.java

--- 1323. Maximum 69 Number.java
+++ 1323. Maximum 69 Number.java
@@ -1,7 +1,6 @@
 class Solution {
   public int maximum69Number(int num) {
     char[] ans = String.valueOf(num).toCharArray();
-
     for (int i = 0; i < ans.length; ++i)
       if (ans[i] == '6') {
         ans[i] = '9';
