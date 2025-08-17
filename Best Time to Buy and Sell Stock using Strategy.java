6e476786744dc4042b9fbf6a38ccd16278f94a6d
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-08-17 08:09:41 +0530

    Update Best Time to Buy and Sell Stock using Strategy.java

--- Best Time to Buy and Sell Stock using Strategy.java
+++ Best Time to Buy and Sell Stock using Strategy.java
@@ -20,28 +20,20 @@ class Solution {
         maxProfit = Math.max(maxProfit, originalProfit + delta);
         for (int start = 1; start + k <= n; start++) {
             int outIdx = start - 1;
-            if (0 < k / 2) { // outgoing was in first half
+            if (0 < k / 2) { 
                 delta -= -(long) strategy[outIdx] * prices[outIdx];
-            } else { // outgoing was in second half
+            } else { 
                 delta -= ((long) 1 - strategy[outIdx]) * prices[outIdx];
             }
-
-            // Shift all indices → instead of re-checking all, just recompute new entering element
             int inIdx = start + k - 1;
-            if (k - 1 < k / 2) { // entering goes to first half
+            if (k - 1 < k / 2) { 
                 delta += -(long) strategy[inIdx] * prices[inIdx];
-            } else { // entering goes to second half
+            } else { 
                 delta += ((long) 1 - strategy[inIdx]) * prices[inIdx];
             }
-
-            // Recompute correction for the boundary shift:
-            // The element at index (start-1 + k/2) changes role:
             int midIdx = start - 1 + k / 2;
-            // Previously it was in second half, now in first half
             delta -= ((long) 1 - strategy[midIdx]) * prices[midIdx];
             delta += -(long) strategy[midIdx] * prices[midIdx];
-
-            // Update max
             maxProfit = Math.max(maxProfit, originalProfit + delta);
         }
 
