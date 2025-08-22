4d31e8afe47da835acb326ff693da16f8096171b
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-08-22 07:33:01 +0530

    Update 3195. Find the Minimum Area to Cover All Ones I.java

--- 3195. Find the Minimum Area to Cover All Ones I.java
+++ 3195. Find the Minimum Area to Cover All Ones I.java
@@ -4,7 +4,6 @@ class Solution {
     int y1 = Integer.MAX_VALUE;
     int x2 = 0;
     int y2 = 0;
-
     for (int i = 0; i < grid.length; ++i)
       for (int j = 0; j < grid[0].length; ++j)
         if (grid[i][j] == 1) {
