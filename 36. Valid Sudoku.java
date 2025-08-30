c69caf7360dd023ddd557d0e0f743144e684a1f0
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-08-30 08:10:56 +0530

    Update 36. Valid Sudoku.java

--- 36. Valid Sudoku.java
+++ 36. Valid Sudoku.java
@@ -1,7 +1,6 @@
 class Solution {
   public boolean isValidSudoku(char[][] board) {
     Set<String> seen = new HashSet<>();
-
     for (int i = 0; i < 9; ++i)
       for (int j = 0; j < 9; ++j) {
         if (board[i][j] == '.')
