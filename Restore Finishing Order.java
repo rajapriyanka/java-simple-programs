b340a00f3f80b49c212188260f798982089246d2
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-08-31 08:02:20 +0530

    Update Restore Finishing Order.java

--- Restore Finishing Order.java
+++ Restore Finishing Order.java
@@ -1,20 +1,15 @@
 class Solution {
     public int[] recoverOrder(int[] order, int[] friends) {
-        // Store friends in a set for quick lookup
         Set<Integer> friendSet = new HashSet<>();
         for (int f : friends) {
             friendSet.add(f);
         }
-        
-        // Collect only friends in the order they appear
         List<Integer> result = new ArrayList<>();
         for (int id : order) {
             if (friendSet.contains(id)) {
                 result.add(id);
             }
         }
-        
-        // Convert List to array
         int[] ans = new int[result.size()];
         for (int i = 0; i < result.size(); i++) {
             ans[i] = result.get(i);
