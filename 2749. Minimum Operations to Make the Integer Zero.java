74bb2105643caf7a186cbee4380e5d5f53ce9b9e
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-09-05 07:48:56 +0530

    Update 2749. Minimum Operations to Make the Integer Zero.java

--- 2749. Minimum Operations to Make the Integer Zero.java
+++ 2749. Minimum Operations to Make the Integer Zero.java
@@ -1,12 +1,6 @@
 class Solution {
   public int makeTheIntegerZero(int num1, int num2) {
-   
-
-
-
-
-
-    for (long ops = 0; ops <= 60; ++ops) {
+       for (long ops = 0; ops <= 60; ++ops) {
       long target = num1 - ops * num2;
       if (Long.bitCount(target) <= ops && ops <= target)
         return (int) ops;
