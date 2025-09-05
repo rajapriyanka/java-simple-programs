fa83e49e05dffd5d59c09cfc4837033c3fea00e6
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-09-05 07:48:11 +0530

    Update 2749. Minimum Operations to Make the Integer Zero.java

--- 2749. Minimum Operations to Make the Integer Zero.java
+++ 2749. Minimum Operations to Make the Integer Zero.java
@@ -2,7 +2,7 @@ class Solution {
   public int makeTheIntegerZero(int num1, int num2) {
    
 
-    // ... + 2^{i_k}), where i_1, i_2, ..., i_k are in the range [0, 60].
+
     // Note that for any number x, we can use "x's bit count" operations to make
     // x equal to 0. Additionally, we can also use x operations to deduct x by
     // 2^0 (x times), which also results in 0.
