f74bc9fd077c1c0ea6bb9800f14cb23d37e6305c
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-09-07 08:31:57 +0530

    Update Minimum Operations to Equalize Array.java

--- Minimum Operations to Equalize Array.java
+++ Minimum Operations to Equalize Array.java
@@ -1,6 +1,5 @@
 class Solution {
     public int minOperations(int[] nums) {
-       
         boolean allEqual = true;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] != nums[0]) {
