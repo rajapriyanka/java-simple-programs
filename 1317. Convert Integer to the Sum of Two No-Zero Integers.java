871c78d3460e428dd4b885506a9e0771e5ed273f
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-09-08 06:44:03 +0530

    Update 1317. Convert Integer to the Sum of Two No-Zero Integers.java

--- 1317. Convert Integer to the Sum of Two No-Zero Integers.java
+++ 1317. Convert Integer to the Sum of Two No-Zero Integers.java
@@ -5,7 +5,6 @@ class Solution {
       if (!String.valueOf(A).contains("0") && !String.valueOf(B).contains("0"))
         return new int[] {A, B};
     }
-
     throw new IllegalArgumentException();
   }
 }
