57c4dd8e71e2e5214f06c803236f56cb1e63242d
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-09-12 07:01:33 +0530

    Update 3227. Vowels Game in a String.java

--- 3227. Vowels Game in a String.java
+++ 3227. Vowels Game in a String.java
@@ -2,7 +2,7 @@ class Solution {
   public boolean doesAliceWin(String s) {
 
 
-    // 3. If k % 2 == 0, Alice wins since Alice can pick (k - 1) vowels,
+
     // then Bob will either pick a substring containing 0 vowels, resulting in
     // Alice picking the remaining entire string, or Bob couldn't pick at all
     // (the last vowel).
