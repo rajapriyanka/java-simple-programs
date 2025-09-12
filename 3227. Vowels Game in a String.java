34d4ea3d2dbb63de5e9db129345ece5a6eb11d47
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-09-12 07:02:33 +0530

    Update 3227. Vowels Game in a String.java

--- 3227. Vowels Game in a String.java
+++ 3227. Vowels Game in a String.java
@@ -2,7 +2,6 @@ class Solution {
   public boolean doesAliceWin(String s) {
     return s.chars().anyMatch(c -> isVowel((char) c));
   }
-
   private boolean isVowel(char c) {
     return "aeiou".indexOf(Character.toLowerCase(c)) != -1;
   }
