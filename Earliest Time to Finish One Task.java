34a2adbcb0a178b971246a6a24f6c8cf51c25688
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-09-14 08:03:59 +0530

    Create Earliest Time to Finish One Task.java

---
+++ Earliest Time to Finish One Task.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int earliestTime(int[][] tasks) {
+        int ans = Integer.MAX_VALUE;
+        
+        for (int[] task : tasks) {
+            int start = task[0];
+            int duration = task[1];
+            int finish = start + duration;
+            ans = Math.min(ans, finish);
+        }
+        
+        return ans;
+    }
+}
