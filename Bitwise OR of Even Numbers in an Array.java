eb8521a29df2e0269018ebd5b72841547acb3c80
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-09-21 09:12:40 +0530

    Update Bitwise OR of Even Numbers in an Array.java

--- Bitwise OR of Even Numbers in an Array.java
+++ Bitwise OR of Even Numbers in an Array.java
@@ -2,7 +2,7 @@ class Solution {
     public int evenNumberBitwiseORs(int[] nums) {
         int result = 0;
         for (int num : nums) {
-            if (num % 2 == 0) {   // check if even
+            if (num % 2 == 0) {  
                 result |= num;   // apply bitwise OR
             }
         }
