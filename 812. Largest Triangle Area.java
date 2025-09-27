085d456b5a4a98596520ab72a0277f181a15e08f
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-09-27 07:39:26 +0530

    Update 812. Largest Triangle Area.java

--- 812. Largest Triangle Area.java
+++ 812. Largest Triangle Area.java
@@ -6,7 +6,6 @@ class Solution {
         for (int[] C : points)
           ans = Math.max(ans, 0.5 * Math.abs((B[0] - A[0]) * (C[1] - A[1]) - //
                                              (C[0] - A[0]) * (B[1] - A[1])));
-
     return ans;
   }
 }
