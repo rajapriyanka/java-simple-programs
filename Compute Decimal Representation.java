c02242a88a0f5d0ef187b3b0eded06026a974ea5
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-09-28 17:33:05 +0530

    Create Compute Decimal Representation.java

---
+++ Compute Decimal Representation.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] decimalRepresentation(int n) {
+        List<Integer> list = new ArrayList<>();
+        int place = 1;
+
+        // Extract components using modulo
+        while (n > 0) {
+            int digit = n % 10;
+            if (digit != 0) {
+                list.add(digit * place);
+            }
+            n /= 10;
+            place *= 10;
+        }
+
+        // Currently list has values in reverse order (smallest place first),
+        // so reverse it to get descending order.
+        Collections.reverse(list);
+
+        // Convert List<Integer> to int[]
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
