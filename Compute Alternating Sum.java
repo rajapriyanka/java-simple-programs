6c447562659ad8672f94c3cd068dca771d47e4ea
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-10-05 08:46:00 +0530

    Create Compute Alternating Sum.java

---
+++ Compute Alternating Sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int alternatingSum(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i % 2 == 0) {
+                sum += nums[i]; // add elements at even indices
+            } else {
+                sum -= nums[i]; // subtract elements at odd indices
+            }
+        }
+        return sum;
+    }
+}
