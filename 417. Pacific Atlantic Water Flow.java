e705124b86be6abdc9864c849c163173ab4c337b
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-10-05 08:53:24 +0530

    Update 417. Pacific Atlantic Water Flow.java

--- 417. Pacific Atlantic Water Flow.java
+++ 417. Pacific Atlantic Water Flow.java
@@ -21,7 +21,6 @@ class Solution {
     }
     bfs(heights, qP, seenP);
     bfs(heights, qA, seenA);
-
     for (int i = 0; i < m; ++i)
       for (int j = 0; j < n; ++j)
         if (seenP[i][j] && seenA[i][j])
