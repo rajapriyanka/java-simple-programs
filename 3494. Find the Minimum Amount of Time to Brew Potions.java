8c4e62969ffc5c68728836aca86d184b5e6097b3
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-10-09 07:18:17 +0530

    Update 3494. Find the Minimum Amount of Time to Brew Potions.java

--- 3494. Find the Minimum Amount of Time to Brew Potions.java
+++ 3494. Find the Minimum Amount of Time to Brew Potions.java
@@ -2,7 +2,6 @@ class Solution {
   public long minTime(int[] skill, int[] mana) {
     long sumSkill = Arrays.stream(skill).sum();
     long prevWizardDone = sumSkill * mana[0];
-
     for (int j = 1; j < mana.length; ++j) {
       long prevPotionDone = prevWizardDone;
       for (int i = skill.length - 2; i >= 0; --i) {
