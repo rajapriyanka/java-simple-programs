cff11f17ac0fc05d4e0d7dff47d1e670bafcc8e3
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-10-18 07:36:31 +0530

    Update 3397. Maximum Number of Distinct Elements After Operations.java

--- 3397. Maximum Number of Distinct Elements After Operations.java
+++ 3397. Maximum Number of Distinct Elements After Operations.java
@@ -2,7 +2,6 @@ class Solution {
   public int maxDistinctElements(int[] nums, int k) {
     int ans = 0;
     int occupied = Integer.MIN_VALUE;
-
     Arrays.sort(nums);
 
     for (final int num : nums)
