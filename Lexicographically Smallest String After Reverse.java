ba30093fa4ea7fa1bf4af84184124a414e02705f
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-10-25 20:18:29 +0530

    Create Lexicographically Smallest String After Reverse.java

---
+++ Lexicographically Smallest String After Reverse.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String lexSmallest(String s) {
+        String smallest = s;
+        int n = s.length();
+
+        for (int k = 1; k <= n; k++) {
+            // Reverse first k characters
+            String revFirst = new StringBuilder(s.substring(0, k)).reverse().toString() + s.substring(k);
+            if (revFirst.compareTo(smallest) < 0) {
+                smallest = revFirst;
+            }
+
+            // Reverse last k characters
+            String revLast = s.substring(0, n - k) + new StringBuilder(s.substring(n - k)).reverse().toString();
+            if (revLast.compareTo(smallest) < 0) {
+                smallest = revLast;
+            }
+        }
+
+        return smallest;
+    }
+}
