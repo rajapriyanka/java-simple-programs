c600a78c1bc242c7d7e1937661a7b281553fdd58
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-10-31 08:17:01 +0530

    Fix array index increment in sneaky numbers logic

--- 3289. The Two Sneaky Numbers of Digitville.java
+++ 3289. The Two Sneaky Numbers of Digitville.java
@@ -4,7 +4,6 @@ class Solution {
     int[] ans = new int[2];
     int[] count = new int[MAX + 1];
     int ansIndex = 0;
-
     for (final int num : nums)
       if (++count[num] == 2)
         ans[ansIndex++] = num;
