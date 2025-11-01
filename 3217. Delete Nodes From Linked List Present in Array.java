ab1bf2db1ba9768a5b55a494e882e0c3a8faa147
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-11-01 17:54:51 +0530

    Update 3217. Delete Nodes From Linked List Present in Array.java

--- 3217. Delete Nodes From Linked List Present in Array.java
+++ 3217. Delete Nodes From Linked List Present in Array.java
@@ -2,7 +2,6 @@ class Solution {
   public ListNode modifiedList(int[] nums, ListNode head) {
     ListNode dummy = new ListNode(0, head);
     Set<Integer> numsSet = Arrays.stream(nums).boxed().collect(Collectors.toSet());
-
     for (ListNode curr = dummy; curr.next != null;)
       if (numsSet.contains(curr.next.val))
         curr.next = curr.next.next;
