31c6336c955fffb34ce534fa03b2a18bdcb0a586
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-11-02 09:27:54 +0530

    Update 2257. Count Unguarded Cells in the Grid.java

--- 2257. Count Unguarded Cells in the Grid.java
+++ 2257. Count Unguarded Cells in the Grid.java
@@ -38,7 +38,6 @@ class Solution {
         else
           down[i][j] = lastCell;
     }
-
     for (int i = 0; i < m; ++i)
       for (int j = 0; j < n; ++j)
         if (grid[i][j] == 0 && left[i][j] != 'G' && right[i][j] != 'G' && up[i][j] != 'G' &&
