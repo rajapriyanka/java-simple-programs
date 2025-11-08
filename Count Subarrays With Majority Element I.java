9ce4426e8bcf52e5d9e041836e8211362a239196
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-11-08 20:32:20 +0530

    Create Count Subarrays With Majority Element I.java

---
+++ Count Subarrays With Majority Element I.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countMajoritySubarrays(int[] nums, int target) {
+        int n = nums.length;
+        int count = 0;
+
+        for (int start = 0; start < n; start++) {
+            int freq = 0;
+            for (int end = start; end < n; end++) {
+                if (nums[end] == target) freq++;
+
+                int length = end - start + 1;
+
+                // Check majority condition
+                if (freq * 2 > length) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+}
