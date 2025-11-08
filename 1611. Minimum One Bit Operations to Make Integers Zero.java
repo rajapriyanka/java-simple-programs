f69f89dff57a42b77f5c35a9e6cc34e4a78ef514
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-11-08 07:34:25 +0530

    Update 1611. Minimum One Bit Operations to Make Integers Zero.java

--- 1611. Minimum One Bit Operations to Make Integers Zero.java
+++ 1611. Minimum One Bit Operations to Make Integers Zero.java
@@ -2,9 +2,6 @@ class Solution {
   public int minimumOneBitOperations(int n) {
     if (n == 0)
       return 0;
-    // x is the largest 2^k <= n.
-    // x | x >> 1 -> x >> 1 needs 1 op.
-    //     x >> 1 -> 0      needs x = 2^k - 1 ops.
     int x = 1;
     while (x * 2 <= n)
       x <<= 1;
