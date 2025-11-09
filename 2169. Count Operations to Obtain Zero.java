06f4f64b4d30c07887d899145882e8f1529278e7
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-11-09 07:45:28 +0530

    Update 2169. Count Operations to Obtain Zero.java

--- 2169. Count Operations to Obtain Zero.java
+++ 2169. Count Operations to Obtain Zero.java
@@ -1,7 +1,6 @@
 class Solution {
   public int countOperations(int num1, int num2) {
     int ans = 0;
-
     while (num1 > 0 && num2 > 0) {
       if (num1 < num2) {
         final int temp = num1;
