575fad772cb4ffa7a3bd3389ea1d3ad51b1e5d3d
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-11-09 08:02:52 +0530

    Create Minimum Distance Between Three Equal Elements I.java

---
+++ Minimum Distance Between Three Equal Elements I.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int minimumDistance(int[] nums) {
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+
+        // Store positions of each number
+        for (int i = 0; i < nums.length; i++) {
+            map.computeIfAbsent(nums[i], x -> new ArrayList<>()).add(i);
+        }
+
+        int minDist = Integer.MAX_VALUE;
+        boolean found = false;
+
+        // Check only elements that appear 3 or more times
+        for (List<Integer> list : map.values()) {
+            if (list.size() >= 3) {
+                found = true;
+                // try every triplet
+                for (int a = 0; a < list.size(); a++) {
+                    for (int b = a + 1; b < list.size(); b++) {
+                        for (int c = b + 1; c < list.size(); c++) {
+                            int i = list.get(a);
+                            int j = list.get(b);
+                            int k = list.get(c);
+
+                            int distance = Math.abs(i - j) + Math.abs(j - k) + Math.abs(k - i);
+                            minDist = Math.min(minDist, distance);
+                        }
+                    }
+                }
+            }
+        }
+
+        return found ? minDist : -1;
+    }
+}
