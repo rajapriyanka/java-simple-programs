6cb476899c8aee0468dc4be5ded713be1c40b042
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-11-25 07:45:37 +0530

    Update 1015. Smallest Integer Divisible by K.java

--- 1015. Smallest Integer Divisible by K.java
+++ 1015. Smallest Integer Divisible by K.java
@@ -2,7 +2,6 @@ class Solution {
   public int smallestRepunitDivByK(int k) {
     if (k % 10 != 1 && k % 10 != 3 && k % 10 != 7 && k % 10 != 9)
       return -1;
-
     Set<Integer> seen = new HashSet<>();
     int n = 0;
 
