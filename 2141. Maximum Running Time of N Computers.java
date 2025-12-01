0589611349ff10f3c41e079d61747b7cfa2fdb1f
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-12-01 08:12:59 +0530

    Update 2141. Maximum Running Time of N Computers.java

--- 2141. Maximum Running Time of N Computers.java
+++ 2141. Maximum Running Time of N Computers.java
@@ -4,9 +4,6 @@ class Solution {
     long sum = Arrays.stream(batteries).asLongStream().sum();
 
     Arrays.sort(batteries);
-
-    // The maximum battery is greater than the average, so it can last forever.
-    // Reduce the problem from size n to size n - 1.
     int i = batteries.length - 1;
     while (batteries[i] > sum / n) {
       sum -= batteries[i--];
