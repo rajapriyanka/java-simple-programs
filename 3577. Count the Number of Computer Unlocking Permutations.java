07d04bd2d6fd7169b1ab3b77be6e6b0af4e1072e
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-12-10 07:37:44 +0530

    Update 3577. Count the Number of Computer Unlocking Permutations.java

--- 3577. Count the Number of Computer Unlocking Permutations.java
+++ 3577. Count the Number of Computer Unlocking Permutations.java
@@ -1,6 +1,5 @@
 class Solution {
     private static final long MOD = 1_000_000_007L;
-
     public int countPermutations(int[] complexity) {
         int n = complexity.length;
         // If any complexity[i] <= complexity[0] for i>0, impossible
