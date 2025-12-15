5f53b04d850151901f1b6b047831b97ed9a2280d
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-12-15 08:04:13 +0530

    Update MoveZerosatTheEnd.java

--- MoveZerosatTheEnd.java
+++ MoveZerosatTheEnd.java
@@ -2,7 +2,6 @@ public class MoveZeros {
     public static void main(String[] args) {
         int[] arr = {0, 1, 0, 3, 12};
         int j = 0; 
-
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != 0) {
                 int temp = arr[j];
