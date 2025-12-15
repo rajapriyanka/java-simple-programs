4962d895ae37859a91b51cd315b95229486b3d2f
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-12-15 08:02:33 +0530

    Create ReverseWords.java

---
+++ ReverseWords.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ReverseWordsSimple {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a sentence:");
+        String sentence = sc.nextLine();
+
+        String[] words = sentence.split(" ");
+        String result = "";
+
+        for (String word : words) {
+            String reversedWord = "";
+            for (int i = word.length() - 1; i >= 0; i--) {
+                reversedWord += word.charAt(i);
+            }
+            result += reversedWord + " ";
+        }
+
+        System.out.println("Reversed words sentence:");
+        System.out.println(result.trim());
+        sc.close();
+    }
+}
