ce4d75e75d3c259b66d1288296d71820e15c0d27
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-12-15 08:03:58 +0530

    Update MoveZerosatTheEnd.java

--- MoveZerosatTheEnd.java
+++ MoveZerosatTheEnd.java
@@ -2,7 +2,7 @@ public class MoveZeros {
     public static void main(String[] args) {
         int[] arr = {0, 1, 0, 3, 12};
 
-        int j = 0; // points to position where next non-zero should go
+        int j = 0; 
 
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != 0) {
