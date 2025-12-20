5f9c0234c11d1e10744bb7acf75d3adb7a2bee69
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-12-20 07:33:54 +0530

    Update 944. Delete Columns to Make Sorted.java

--- 944. Delete Columns to Make Sorted.java
+++ 944. Delete Columns to Make Sorted.java
@@ -1,7 +1,6 @@
 class Solution {
   public int minDeletionSize(String[] strs) {
     int ans = 0;
-
     for (int j = 0; j < strs[0].length(); ++j)
       for (int i = 0; i + 1 < strs.length; ++i)
         if (strs[i].charAt(j) > strs[i + 1].charAt(j)) {
