1176477b10bccb4221069b3ce1d80ca146b42b66
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-12-24 07:24:40 +0530

    Update 3074. Apple Redistribution into Boxes.java

--- 3074. Apple Redistribution into Boxes.java
+++ 3074. Apple Redistribution into Boxes.java
@@ -2,7 +2,6 @@ class Solution {
   public int minimumBoxes(int[] apple, int[] capacity) {
     final int appleSum = Arrays.stream(apple).sum();
     int capacitySum = 0;
-
     Arrays.sort(capacity);
 
     for (int i = 0; i < capacity.length; ++i) {
