151a81a2529a256940d2155409fa472a3eadddfa
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-12-26 07:10:35 +0530

    Update Prefix Sum String 2483. Minimum Penalty for a Shop.java

--- Prefix Sum String 2483. Minimum Penalty for a Shop.java
+++ Prefix Sum String 2483. Minimum Penalty for a Shop.java
@@ -3,7 +3,6 @@ class Solution {
         int ans = 0;
     int profit = 0;
     int maxProfit = 0;
-
     for (int i = 0; i < customers.length(); ++i) {
       profit += customers.charAt(i) == 'Y' ? 1 : -1;
       if (profit > maxProfit) {
