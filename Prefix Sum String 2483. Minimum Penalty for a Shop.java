f49089360c50e31ece049be46622afb3b73ef8ac
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2025-12-26 07:10:58 +0530

    Update Prefix Sum String 2483. Minimum Penalty for a Shop.java

--- Prefix Sum String 2483. Minimum Penalty for a Shop.java
+++ Prefix Sum String 2483. Minimum Penalty for a Shop.java
@@ -1,6 +1,6 @@
 class Solution {
   public int bestClosingTime(String customers) {
-        int ans = 0;
+    int ans = 0;
     int profit = 0;
     int maxProfit = 0;
     for (int i = 0; i < customers.length(); ++i) {
