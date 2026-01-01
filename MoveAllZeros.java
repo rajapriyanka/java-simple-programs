2702b755f54e868ab28bcea4e316663f456db045
Rajapriyanka <122675666+rajapriyanka@users.noreply.github.com>
2026-01-01 15:53:17 +0530

    Create MoveAllZeros.java

---
+++ MoveAllZeros.java
@@ -0,0 +1,31 @@
+package main;
+
+import java.util.Scanner;
+
+public class MoveAllZeros {
+	public static void zeros(int arr[]) {
+		int index =0;
+		int result[] = new int[arr.length];
+		for(int i=0;i<arr.length;i++) {
+			if(arr[i]!=0) {
+				result[index++]=arr[i];
+			}
+		}
+		for(int num:result) {
+			System.out.print(num+" ");
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i =0; i<n;i++) {
+			arr[i]= sc.nextInt();
+		}
+		zeros(arr);
+		
+	}
+
+}
